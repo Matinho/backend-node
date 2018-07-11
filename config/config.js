a705d9c739e380bd4374f078040aeb256734ffe8
Matias Figueredo <MFigueredo@aconcaguasf.com>
2018-07-11 17:11:36 -0300

    Servidor Backend con autenticación de google terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@Este-es@-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '487626411064-5smo09j3rk0jjafb0o5q97iccak9shv1.apps.googleusercontent.com'
--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,121 @@ var app = express();
 
 var Usuario = require('../models/usuario'); // estoy definiendo el modelo de usuario
 
+// Google
+var CLIENT_ID = require('../config/config').CLIENT_ID; // obtengo el seed 
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+// ================================
+// Autenticación de Google
+// ================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true,
+    };
+}
+
+app.post('/google', async(req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(403).json({
+                ok: false,
+                mensaje: "Token no valido"
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) { // si tengo error mando un msj de error
+            return res.status(500).json({
+                ok: false,
+                mensaje: "Error al buscar Usuarios",
+                errors: err
+            });
+        }
+
+        // Verifico que el usuario exista 
+        if (usuarioDB) {
+            // si el correo esta usado pero no creado por google tiro error
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: "Debe de usar su atenticación normal",
+                    errors: err
+                });
+            }
+            // si esta creado pero fue creado por google genero el nuevo token de acceso
+            else {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }) // 14400 son 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            }
+
+        }
+        // si el usuario no existe, lo creamos
+        else {
+            var usuario = new Usuario();
+
+            // seteo el usuario
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            // grabo el usuario
+            usuario.save((err, usuarioDB) => {
+
+                if (err) { // si tengo error mando un msj de error
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: "Error al grabar Usuario",
+                        errors: err
+                    });
+                }
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }) // 14400 son 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+            });
+        }
+
+    });
+});
+
+
+
+// ================================
+// Autenticación Normal
+// ================================
 app.post('/', (req, res) => {
 
 
